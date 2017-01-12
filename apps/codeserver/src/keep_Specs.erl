4860510b600a0ada0f85e2bed5034fb30ce3fdd7
Koustubh Shama <koustubh.sharma@ericsson.com>
2017-01-12 16:42:16 +0100

    adding terminateprocess in keep_Specs and functions to make it look clean merge to codeserverv1 branch

--- apps/codeserver/src/keep_Specs.erl
+++ apps/codeserver/src/keep_Specs.erl
@@ -1,14 +1,10 @@
 -module(keep_Specs).
--export([start/0, add/2,delete/2,fetch/3]).
-%% -define(ModuleName_r_List= []).
-%% -define(FunctionName_r_List= []).
-%% -define(ModuleName_ur_List= []).
-%% -define(FunctionName_ur_List= []).
-
+-export([start/0, add/2,delete/2,fetch/0,terminate_process/0]).
 
 start()->
     Ref = make_ref(),
     Parent = self(),
+    io:format("Starting"),
     spawn(fun() ->		  
 		  register(?MODULE, self()),
 		  Parent ! {Ref, started},
@@ -20,27 +16,22 @@ start()->
     after 1000 ->
 	    {error, "Couldn't start keep_Specs"}
     end.
-	   
-
 
 loop(Old_List)->
-    
     receive 
 	{From, Ref, add,Module,Function} ->
 	    From ! {Ref, {ok,added}},
 	    loop([{Module,Function}|Old_List]);	
-	
 	{From,Ref,delete,Module, Function} ->
 	    New_List = delete_from_list(From,Ref,Module, Function,Old_List),
 	    loop(New_List);
-	
 	{From,Ref,fetch} ->
-	    fetch(From,Ref,Old_List),
+	    do_fetch(From,Ref,Old_List),
 	    loop( Old_List);
-	terminate ->
+	{From,Ref,terminate} ->	    
+	   From !{Ref,terminated},
 	    ok
     end.
-
 
 add(Module, Function) ->
     Ref = make_ref(),
@@ -64,7 +55,7 @@ delete(Module, Function) ->
 
 delete_from_list(From,Ref,Module,Function,Old_List)->
     case lists:member({Module,Function},Old_List) of
-		true ->
+	true ->
 	    From ! {Ref,{ok,deleted}},
 	    lists:delete({Module,Function},Old_List);
 	false ->
@@ -72,60 +63,27 @@ delete_from_list(From,Ref,Module,Function,Old_List)->
 	    Old_List
     end.
 
-fetch(From,Ref,Old_List)->
+do_fetch(From,Ref,Old_List)->
     From !{Ref,{ok,Old_List}}.
 
 
+fetch() ->
+    Ref = make_ref(),
+    ?MODULE ! {self(),Ref,fetch},
+    receive 
+	{Ref, Res} ->
+	    Res
+    after 500 ->
+	    {error, timeout}
+    end.
 
-%% loop()->
-    
-%%     update_database(),    
-    
-%%     loop().
-
-
-%% update_database(Mod_name, Func_name) ->
-%%     receive
-%%         {From,add } ->
-%%             From ! {self(), ok};
-           
-%%         {From,delete } ->
-%%             case lists:member(List) of
-%%                 true ->
-%%                     From ! {self(), {ok }},
-%%                     f(lists:delete(List));
-%%                 false ->
-%%                     From ! {self(), not_found},
-%%                     f(List)
-%%             end;
-%%         terminate ->
-%%             ok
-%%     end.
-
-
-
-
-%% update_restrict_database(ModuleName,FunctionName)->
-%%     ModuleName_r_List = [ModuleName|ModuleName_r_List],
-%%     FunctionName_r_List1 = [FunctionName|FunctionName_r_List],
-%%     {restrict,ModuleName_r_List,FunctionName_r_List}.    
-
-
-%% update_un_restrict_database(ModuleName,FunctionName)->
-%%     [ ModuleName | ModuleName_ur_List],
-%%     [ FunctionName |FunctionName_ur_List],
-    
-%%     {un_restrict,ModuleName_ur_List,FunctionName_ur_List}.
-
-%% update_restrict_database([H|New_Files],Old_Files)->
-%%     case  lists:member(H,Old_Files) of
-%% 	false ->
-%% 	    Old_Files_appended = lists:append(H,Old_Files),
-%% 	    createMemoryFunction(New_Files,Old_Files_appended);
-	
-%% 	_-> createMemoryFunction(New_Files,Old_Files)
-%%     end;
-%% update_restrict_database([],Old_Files) ->
-%%     Old_Files.
-
-
+terminate_process()->
+    Ref=make_ref(),
+    ?MODULE ! {self(),Ref,terminate},
+    receive 
+	{Ref,terminated}->
+	    timer:sleep(100),
+	    ok
+    after 500 ->
+	    {error,timeout}
+    end.
