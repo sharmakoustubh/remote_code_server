8f940724e9d36676707aeceab12269af24c889e6
Koustubh Shama <koustubh.sharma@ericsson.com>
2017-02-28 11:01:41 +0100

    commit on 28 feb 2017 11:02 27 tests are passing catching the EXIT error in parse

--- apps/codeserver/src/clientListener.erl
+++ apps/codeserver/src/clientListener.erl
@@ -2,16 +2,14 @@
 -define(Complements,[{$",$"},{$',$'},{$[,$]},{${,$}},{$\",$\"}]).
 -export([is_restricted/3,
 	 start/0,
-	 check_format/1,
 	 parse/1,
-	 parse_args/1,
 	 execute_run/1,
 	 execute_info/0,
 	 execute_list/0,
 	 to_expression/1,
-	 read_data/1,
-	 get_block_and_remainder/3
-%%	 to_erlang_string/1
+	 tokenise_data_for_n_terms/3,
+	 format/1,
+	 process/2
 	]).
 
 -include("record_definition.hrl").
@@ -51,7 +49,7 @@ do_recv(Sock) ->
     case gen_tcp:recv(Sock, 0) of
 	{ok, Data} ->
 	    io:format("Got data: ~p~n", [Data]),
-	    check_process_send(Data,Sock),
+	    format_process_send(Data,Sock),
 	    do_recv(Sock);
 	{error, closed} ->
 	    io:format("Connection closed~n", []);
@@ -60,37 +58,40 @@ do_recv(Sock) ->
     end.
 
 
-check_process_send(Data,Sock)->
-    Formatted_data = check_format(Data),
-    Output = case catch process(Formatted_data,Sock) of
-		 {'EXIT',Error}->
-		     {error,Error};
+format_process_send(Data,Sock)->
+    Formatted_data = format(Data),
+    Output = case  process(Formatted_data,Sock) of
+		 {error,Error}->
+		     Error;
 		 Result->
-		    Result 
-	     end,
-
-%%     io:format(user,"tcp send data Result~p~n",[Output]),
+		     Result 
+	     end,    
     send(Sock,Output).
 
-check_format(Data)->
-    Data_without_nextline = Data -- "\n",
-    Formatted_data = read_data(Data_without_nextline),
-    string:tokens(Formatted_data,","). 
+format(Data)->
+    case "\n" == Data of
+	true ->
+	    Data;
+	false ->
+	    Data -- "\n"
+    end.
 
 process(Data,Sock)->
-    Count_elements = count(Data,0),
-    case hd(Data) of
+    io:format(user,"~p~n",[Data]),
+    [Cmd|Rest] = string:tokens(Data," "),
+    Count_elements = length(Rest),
+    case Cmd of
     	"list"->
 	    case Count_elements of
-		1->
+		0->
 		    execute_list();
 		_->
 		    ?INPUT_ERROR
 			
 	    end;
 	
-	"run"->
-	    case Count_elements >= 3 of
+	"run"->	    
+	    case Count_elements >= 2 of
 		true ->
 		    execute_run(Data);	
 		
@@ -100,7 +101,7 @@ process(Data,Sock)->
 	
 	"info"->
 	    case Count_elements of
-		1->
+		0->
 		    execute_info();
 		_->
 		    ?INPUT_ERROR
@@ -108,16 +109,16 @@ process(Data,Sock)->
 
 	"exit"->
 	    case Count_elements of
-		1->
+		0->
 		    
 		    terminate(Sock);
 		_->
 		    ?INPUT_ERROR
 	    end;
     	_->
-    	    "This command is not executable; Executable commands are list,run,info,exit"
+    	    lists:flatten(io_lib:format("~s~n",["This command is not executable; Executable commands are list,run,info,exit"]))
     end.
-
+    
 
 execute_list()->
     case file_handler:fetch() of 
@@ -126,14 +127,8 @@ execute_list()->
 	{error,Error}->
 	    Error;
 	{ok,List}->
-	    %% Res = [{Key,Rec#module.exported}||{Key,Rec}<-List],
-	    %% Formatted_output = format_result(Res,[]),
-       
 	    Res = [pretty_print({Key,Rec#module.exported}) ||{Key,Rec}<-List],
-	    Res_format = lists:flatten(io_lib:  format("~s~n",[Res])),
-	   %% io:format(user,"~s~n", [Res_format])
-	    Res_format
-	%%    [lists:flatten (io_lib:format(" ~p~n  ~p",[Key,Function_names]))||{Key,Function_names} <- Formatted_output] 	    
+	    lists:flatten(io_lib:format("~s~n",[Res]))
     end.
 
 
@@ -143,8 +138,7 @@ pretty_print(H) ->
     Function_format_list = [lists:flatten (io_lib:format("  ~p/~p~n",[Fun, Arity]))|| {Fun, Arity} <- Funs],
     io:format(user,"~p~n",[Function_format_list]),
     Function_format_flat = lists:flatten(io_lib:format("~s~n",[Function_format_list])),
-%%    io_lib:format(user,"~s~n",Module_format ++ Function_format_flat).
-      Module_format ++ Function_format_flat.
+    Module_format ++ Function_format_flat.
 
 
 execute_info()->
@@ -155,9 +149,7 @@ execute_info()->
 	    Error;
 	{ok,List}->
 	   Res = [pretty_print_info({Key,Rec#module.module_md5,Rec#module.compile_time})||{Key,Rec}<-List],
-	%%    [lists:flatten (io_lib:format("For Module ~p the md5 is ~p Compiled on date ~p-~p-~p at time ~p:~p:~p~n",[Key,Md5,Date,Month,Year,Hrs,Min,Seconds]))||{Key,Md5,{Year,Month,Date,Hrs,Min,Seconds}} <- Res]
-	    Res_format = lists:flatten(io_lib:  format("~s~n",[Res])),
-	    Res_format  
+	    lists:flatten(io_lib:format("~s~n",[Res]))
     end.
 
 
@@ -165,30 +157,65 @@ pretty_print_info(H) ->
     {Mod_name,Md5,{Year,Month,Date,Hrs,Min,Seconds}}=H, 
     Module_format = lists:flatten (io_lib:format("~s~n",[Mod_name])),
     Function_format_list = [lists:flatten (io_lib:format(" the md5 is ~p Compiled on date ~p-~p-~p at time ~p:~p:~p~n",[Md5,Year,Month,Date,Hrs,Min,Seconds]))],
-
-
     io:format(user,"~p~n",[Function_format_list]),
     Function_format_flat = lists:flatten(io_lib:format("~s~n",[Function_format_list])),
-%%    io_lib:format(user,"~s~n",Module_format ++ Function_format_flat).
-      Module_format ++ Function_format_flat.
+    Module_format ++ Function_format_flat.
 
 
-
-    
 execute_run(Data)->
-    {ok,Modules} = file_handler:fetch(),
-    {Module,{Function,Arity}} = parse(Data),
-    Result =   run_seq([fun module_exists/3,
-			fun is_exported/3,
-			fun is_restricted/3
-		       ],{ok,Module,{Function,Arity},Modules}),
-    case Result of
-	{ok,"The Function is un-restricted !!!"} ->
-	    Run_fun_res =  run_fun(Module,Function,Data),
-	    Formatted_data = io_lib:format("~p~n",[Run_fun_res]),
-	    Formatted_data;
-        {error,Msg}->
-	    Msg
+    {ok, Modules} = file_handler:fetch(),
+    case catch parse(Data) of
+	{'EXIT', Error}->
+	    Error;
+	{Module, {Function, Arity}, Args_list}->
+	    Result = run_seq([fun module_exists/3,
+			      fun is_exported/3,
+			      fun is_restricted/3
+			     ],{ok,Module,{Function,Arity},Modules}),
+	    case Result of
+		{ok,"The Function is un-restricted !!!"} ->
+		    Run_fun_res =  run_fun(Module,Function,Args_list),
+		    io_lib:format("~p~n",[Run_fun_res]);	    
+		{error,Msg}->
+		    Msg
+	    end
+    end.
+
+parse(Data)->
+    io:format(user,"Data is .......> ~p ~n", [Data]),
+    [ _, Module, Function | Args] = tokenise_data_for_n_terms(Data,[],3),
+    io:format(user,"mod ~p fun ~p args are ~p ~n", [Module,Function,Args]),
+    Args_list = to_expression("["++Args++"]"),
+    Args_number = length(Args_list),
+    Function_atom =  list_to_atom(Function),
+    io:format(user,"Parse results are---->>>>> mod ~p fun ~p  args list ~p  Args Number  ~p ~n", [Module,Function_atom,Args_list,Args_number]),
+    {Module,{Function_atom,Args_number},Args_list}.
+
+tokenise_data_for_n_terms(T,Acc,0)->
+%%    {string:tokens(lists:reverse(Acc),","),T},
+    lists:append(string:tokens(lists:reverse(Acc),","),T);
+
+tokenise_data_for_n_terms([H|T],Acc,Max)->
+    case H of
+	$\s ->
+	    tokenise_data_for_n_terms(T,[$,|Acc],Max-1);
+	_ ->
+	    tokenise_data_for_n_terms(T,[H|Acc],Max)
+    end.
+
+to_expression(String) ->
+    io:format(user,"to expression we got ~p ~n ",[String]),
+    {ok, Tokens,_ } = erl_scan:string(String ++ "."),
+    {ok, Abstract} = erl_parse:parse_exprs(Tokens),
+    {value, Expression, _} = erl_eval:exprs(Abstract, erl_eval:new_bindings()),
+    Expression.
+
+run_fun(Module,Function,Args_list)->
+    case catch apply(list_to_atom(Module), Function, Args_list) of	
+    	{'EXIT',Error} ->
+    	    {error,Error};
+    	Res->
+    	    Res		
     end.
 
 
@@ -202,7 +229,6 @@ run_seq(_,{error,Msg}) ->
 module_exists(Module,Function,Modules)->
     case lists:member(Module,proplists:get_keys(Modules)) of
 	true ->
-	  %%io:format(user,"the module_exists is TRUE~p~n",[true]),
 	    {ok,Module,Function,Modules};
 	false ->
 	    {error,"The Module does not exist !!!"}
@@ -211,14 +237,10 @@ module_exists(Module,Function,Modules)->
 is_exported(Module,Function,Modules)->
     Record = proplists:get_value(Module,Modules),
     Exported_funs =Record#module.exported,
-%%  io:format(user,"the passed function to the module is ~p~n",[Function]),
-      %%io:format(user,"the Exported function  to the module is ~p~n",[Exported_funs]),
     case lists:member(Function,Exported_funs) of
 	true ->
-	   %%o:format(user, "the Is_Exported is TRUE~p~n",[true]),
 	    {ok,Module,Function,Modules};
 	false ->
-	%%   io:format(user,"the Funcs --- does not exist return tuple error it does not exist ~p~n",[func_doesnt_exist]),
 	    {error,"The Function does not exist !!!"}
     end.
 
@@ -227,292 +249,15 @@ is_restricted(Module,Function,Modules)->
     Restricted = file_handler:get_restricted(Module, Modules),
     case lists:member(Function,Restricted) of
 	true ->
-	  %%   io:format(user, "the Is_Restricted is TRUE~p~n",[true]),
 	    {error,"The Function is restricted !!!"};
 	false ->
-	%%    io:format(user, "the Is_Restricted is FALSE~p~n",[false]),
-
 	    {ok,"The Function is un-restricted !!!"}		
     end.
 
-run_fun(Module,Function,Data)->
-    case catch apply(list_to_atom(Module), Function, parse_args(Data)) of	
-    	{'EXIT',Error} ->
-    	    {error,Error};
-    	Res->
-    	    Res		
-    end.
-
-
-
-parse(Data)->
-   %% io:format(user,"Data ~p ~n", [Data]),
-    [ _, Module, Function | Args] = Data,
-    io:format(user,"Data is .......> ~p ~n", [Data]),
-    Args_number = count(Args,0),
-    Function_atom =  list_to_atom(Function),
-    io:format(user,"Parse ~p ~p ~p ~n", [Module,Function_atom,Args_number]),
-    {Module,{Function_atom,Args_number}}.
-
-parse_args(Data)->
-    [ _, _Module, _Function | Args] = Data,
-    [to_expression(Arg)||Arg<- Args ].
 
 terminate(Sock)->
     gen_tcp:close(Sock).
 
 send(Sock,Result)->
-    %%  Formatted_data = io_lib:format("~p~n",[Result]),
     gen_tcp:send(Sock, Result).
 
-count([],Counter)->
-    Counter;
-count([_H|T],Counter)->
-    count(T,Counter+1).
-
-to_expression(String) ->
-    {ok, Tokens,_ } = erl_scan:string(String ++ "."),
-    {ok, Abstract} = erl_parse:parse_exprs(Tokens),
-    {value, Expression, _} = erl_eval:exprs(Abstract, erl_eval:new_bindings()),
-    Expression.
-
-%% to_erlang_string(String) ->
-%%     "[" ++
-%%     [begin
-%% 	 case C of
-%% 	     $\s -> $,;
-%% 	     $"->
-%% 		 check_for_double_quotes_ending()
-%% 	     _ -> C
-%% 	 end
-%%      end || C <- String] ++ "]".
-
-%% double([]) ->
-%%     [];
-%% double([H|T]) ->
-%%     [H*2 | double(T)].
-
-
-
-%% separate_quoted_non_quoted([H|T])->
-%%     case H of
-%% 	$" 
-
-%% check_string_is_proper([H|T],) ->
-%%     case is_special(H) of
-%% 	true ->
-%% 	    Ending = get_ending_character(H),
-%% 	    case does_special_char_ends(Ending,T) of
-%% 		true->
-%% 		    ;
-%% 		false ->
-%% 	    end;
-%% 	false ->
-%% 	    check_string_is_proper([H|Heading],Quoted,Trailing,T)
-%%     end.
-
-is_special(Char)->
-    lists:member(Char, "\"{['").
-
-get_ending_character(H) ->
-    proplists:get_value(H,?Complements).
-
-does_special_char_ends(Char,[])-> 
-    false;
-
-does_special_char_ends(Char,[H|T])-> 
-    case H of
-	Char ->
-	    true;
-	_ ->
-	    does_special_char_ends(Char,T)
-    end.
-
-get_block_and_remainder([], _, _) ->
-    {error, "Not a valid block"};
-get_block_and_remainder([C|T], C, Acc) ->
-    {ok, {Acc, T}};
-get_block_and_remainder([H|T], C, Acc) ->
-    get_block_and_remainder(T, C, [H|Acc]).
-
-read_data(Text) ->
-    read_data([], [], [], Text).
-
-read_data(Heading,Word_Block,Trailing,[])->
-    {lists:reverse(Heading),lists:reverse(Word_Block),lists:reverse(Trailing)},
-    lists:reverse(Heading);
-
-read_data(Heading,Word_Block,Trailing,[H|T])->
-    case is_special(H) of
-	true ->
-	    Ending = get_ending_character(H),
-	    io:format(user,"H is ~p ~n",[H]),
-	    case get_block_and_remainder(T,Ending,[]) of
-		{ok,{Within_special_chars,After_special_char}}->
-		    Wrapped = wrap(Within_special_chars,H,Ending),
-		    Joint_string = Wrapped++Heading,
-		    read_data(Joint_string,Wrapped,After_special_char,After_special_char);
-		   %% [Joint_string,Wrapped,After_special_char,H,Ending];
-		Error ->
-		    Error
-	    end;
-	false ->
-	    case H of
-		$\s -> 
-		    read_data([$,|Heading],Word_Block,Trailing,T);
-		_ ->
-		    io:format(user,"Heading ~p Word_Block ~p trailing ~p ~n",[(Heading),(Word_Block),(Trailing)]),
-		    read_data([H|Heading],Word_Block,Trailing,T)
-	    end
-    end.
-
-
-wrap(S,Start,End) ->
-    [End|S] ++ [Start].
-
-
-%% 	    check_string_is_proper([H|Heading],Word_Block,Trailing,T)
-%% end.
-
-
-
-%% case H of
-%% 	$" ->
-%% 	    case check_for_double_quotes_ending([],T) of
-%% 		{ok,{Quoted_word,Trailing}}->
-%% 		    io:format(user,"Heading ~p Quoted ~p trailing ~p ~n",[lists:reverse(Heading),lists:reverse(Quoted),lists:reverse(Trailing)]),
-    %% 		    read_data(Quoted_word++Heading,Quoted_word,Trailing,Trailing);
-%% 		{error,Error} ->
-%% 		    Error
-%% 	    end;
-%% 	$\s -> 
-%% 	    read_data([$,|Heading],Quoted,Trailing,T);
-
-%% 	_ ->
-%% 	    io:format(user,"Heading ~p Quoted ~p trailing ~p ~n",[lists:reverse(Heading),lists:reverse(Quoted),lists:reverse(Trailing)]),
-%% 	    read_data([H|Heading],Quoted,Trailing,T)
-%% end.
-
-
-check_for_double_quotes_ending(_,[])->
-    {error, "There is no ending quote"};
-
-check_for_double_quotes_ending(Acc,[H|T])->
-    case H of
-	$" ->
-	    {ok,{Acc,T}};
-	_->
-	    check_for_double_quotes_ending([H|Acc],T)
-    end.
-
-
-
-%% separate_string([H|T],outside,Counter)->
-%%     case special(H) of
-%% 	true ->
-%% 	  separate_string(T,inside,Counter);
-%% 	false ->
-%% 	   [H|separate_string(T,outside,Counter)]
-%%     end.
-
-
-
-%% separate_string([H|T], inside, Counter)->
-%%     case special(H) of
-%% 	true->
-%% 	   separate_string(T,inside,Counter);
-%% 	false ->
-%% 	    [H|separate_string(T,inside,Counter)]
-%%     end.
-
-
-
-
-%% separate_string([H|T],inside,Counter)->
-%% 			case special(H) of
-%% 			    true ->
-%% 				separate_string(T,inside,Counter2),
-%% 				;
-%% 			    false ->
-%% 				[H|separate_string(T,inside,Counter)]
-%% 			end
-	
-			    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    %% 	$[ ->
-    %% 	    {Rem_string, Acc} = check_for_square_braces_ending(T),
-    %% 	    read_data(Rem_string,Acc);
-    %% 	${ ->
-    %% 	    {Rem_string, Acc} = check_for_curly_braces_ending(T),
-    %% 	    read_data(Rem_string,Acc);
-		    
-    %% 	$' ->
-    %% 	    {Rem_string, Acc} = check_for_single_quotes_ending(T),
-    %% 	    read_data(Rem_string,Acc);
-	    
-    %% 	$\" ->	  
-    %% 	    {Rem_string, Acc} = check_for_slash_ending(T),
-    %% 	    read_data(Rem_string,Acc);
-	   
-    %% 	$\' ->
-    %% 	    {Rem_string, Acc} = check_for_single_quotes_ending(T),
-    %% 	    read_data(Rem_string,Acc);
-    %% 	 _ ->
-    %% 	    ignore
-    %% end.
-
-%% check_for_double_quotes_ending(T,Acc)->
-%%     [Ele+Acc||Ele <- T],
-
-%% check_for_square_braces_ending(Word,Acc)->
-%%     [case Letter of
-%% 	 ] ->
-%% 						;
-%% 	_->
-%% 						New_Acc = Letter++Acc
-
-%%  || Letter<- Word]
-%%     .
-
-%% %% Keys =  proplists:get_keys(List),	    
-%% 	    %% Rec_List =  [proplists:get_value(Key,List)||Key <-Keys],
-%% 	    %% Md5s = [Rec#module.module_md5||Rec <- Rec_List],
-%% 	    %% Compile_times = [Rec#module.compile_time||Rec <- Rec_List],
-%% 	    %% lists:merge3(Keys,Md5s,Compile_times)
--- apps/codeserver/test/clientListener_tests.erl
+++ apps/codeserver/test/clientListener_tests.erl
@@ -9,17 +9,11 @@ clientListener_test_() ->
      fun cleanup/1,
      [
       {"check you can start the Client server",fun start_server/0},
-      {"check the input format is correct",fun check_format/0},
       {"check parsing is done correct",fun check_parse/0},
       {"check port is already in use",fun port_already_in_use/0},
-      {"check parsing args works",fun check_parse_args/0},
-      {"test 1 of reading data and parsing the args",fun read_data1/0},
-      {"test 2 of reading data and parsing the args ",fun read_data2/0},
-      {"test 3 of reading data and parsing the args ",fun read_data3/0},
-      {"test 4 of reading data and parsing the args ",fun read_data4/0},
-  {"test 5 of reading data and parsing the args ",fun read_data5/0}
+      {"check parsing args works",fun parse_should_give_correctly_ordered_mod_funs_args/0}
+      
      ]}.
-
 
 setup() ->
     ok = file_handler:start(),
@@ -60,190 +54,22 @@ start_server()->
     io:format("~p~n",[Pid]),
     ?assertMatch(true,is_pid(Pid)).
 
-check_format()->
-    Expect = ["run","ets_table1","return_ok"], %% mODULE IS IN STRING AND fUNCTION IS IN ATOM,
-    Result = clientListener:check_format("run ets_table1 return_ok"),
+parse_should_give_correctly_ordered_mod_funs_args()->
+    Expect = {"robin",{seq,2},[3,10]}, 
+    Result = clientListener:parse("run robin seq 3,10"),
     ?assertMatch(Expect, Result).
 
-%% check_format_with_quoted_variables()->
-%%     Expect = ["run","ets_table1","greet_me","\"ThisIsQuoted\""],
-%%     Result = clientListener:check_format("run ets_table1 greet_me \"ThisIsQuoted\""),
-%%     ?assertMatch(Expect, Result).
-
 check_parse()->
-    Expect = {"ets_table1",{my_fun,2}},
-    Result = clientListener:parse(["run","ets_table1","my_fun","a","b"]),
+    Expect = {"are",{you,2},[a, b]},
+    Result = clientListener:parse("how are you a,b"),
     ?assertMatch(Expect,Result).
 
-check_parse_args()->
-    Expect = ["Sharma"],
-    Result = clientListener:parse_args(["run","ets_table1","greet_me","\"Sharma\""]),
-    ?assertMatch(Expect,Result).
-
-read_data1()->
-    Input = "a bb ccc {Koustubh Sharma}",
-    Expect = "a,bb,ccc,{Koustubh Sharma}",
-    Result = clientListener:read_data(Input),
-    ?assertMatch(Expect,Result).
-
-read_data2()->
-    Input = "a bb ccc \"Koustubh Sharma\"",
-    Expect = "a,bb,ccc,\"Koustubh Sharma\"",
-    Result = clientListener:read_data(Input),
-    ?assertMatch(Expect,Result).
-
-read_data3()->
-    Input = "a bb ccc \"Koustubh Sharma\" james bond",
-    Expect = "a,bb,ccc,\"Koustubh Sharma\",james,bond",
-    Result = clientListener:read_data(Input),
-    ?assertMatch(Expect,Result).
-read_data4()->
-    Input = "a bb ccc \"Koustubh Sharma\" \"Hello World\"",
-    Expect = "a,bb,ccc,\"Koustubh Sharma\",\"Hello World\"",
-    Result = clientListener:read_data(Input),
-    ?assertMatch(Expect,Result).
-read_data5()->
-    Input = "\"Koustubh Sharma\" a b {\"Hello World\" c}",
-    Expect = "\"Koustubh Sharma\",a,b,{\"Hello World\" c}",
-    Result = clientListener:read_data(Input),
+tokenise_data_for_n_terms()->
+    Input = "please tokenise tillhere \"dont tokenise\" this",
+    Expect = ["please","tokenise","tillhere","\"dont tokenise\" this"],
+    Result = clientListener:tokenise_data_for_n_terms(Input,[],3),
     ?assertMatch(Expect,Result).
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% check_execute_list()->
-
-%%     [Result] = clientListener:execute_list(),
-%%     io:format(user,"the result of execute info~p~n",[Result]),
-%%     Res = re:run(Result,"For Module \"ets_table1\" the Exported Functions are  [start,return_ok,greet_me,\n                                              module_info,module_info]"),
-%%     ?assertMatch({match,_},Res).
-
-%% check_execute_run()->
-%%     Expect = "Hello Koustubh",
-%%     Result = clientListener:execute_run(["run","ets_table1","greet_me","Koustubh"]),
-%%     ?assertMatch(Expect,Result).
-
-%% when_execute_run_should_crash()->
-%%  %%   Result = clientListener:execute_run(["run","ets_table1","greet_me",]),
-
-%%     Result = clientListener:execute_run(["run","ets_table1","return_ok",abcd]),
-%%     ?assertMatch({error,_},Result).
-
-%% check_execute_info_works()->
-%%     Result = clientListener:execute_info(),
-%%     io:format(user,"the result of execute info~p~n",[Result]),
-%%     Res = re:run(Result,"For Module \".*\"  the md5 is \".*\" Compiled on date .* at time .*"),
-%%     ?assertMatch({match,_},Res).
-
-%% parse_test_() ->
-%%     [{"Converting to a simple erlang string", fun to_erlang_string_1/0}].
-
-%% to_erlang_string_1()->
-%%     Input = "a b c",
-%%     Expected = "[a,b,c]",
-%%     Result = clientListener:to_erlang_string(Input),
-%%     ?assertMatch(Expected, Result).
-
-%% to_erlang_string_2()->
-%%     Input = "a b c \"Quote afterspace\"",
-%%     Expected = "[a,b,c,\"Quote afterspace\"]",
-%%     Result = clientListener:to_erlang_string(Input),
-%%     ?assertMatch(Expected, Result).
-
-%% check_string_is_proper_1()->
-%%     Input = "run ets_table1 greet_me \"Koustubh Sharma\"",
-%%     Result = clientListener:check_string_is_proper(Input),
-%%     ?assertMatch(true, Result).
-    
-    
-%% 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% perform_clientListener_test_() ->
-%%     {foreach,
-%%      fun setup/0,
-%%      fun cleanup/1,
-%%      [fun function_is_restricted/0,
-%%       fun function_is_not_restricted/0
-%%      ]}.
-
-%% setup()->
-%%  %keep_Specs:start().
-
-%% cleanup(_)->
-  
-%%     %keep_Specs:terminate_process().
-
-
-%% function_is_not_restricted()->   
-%%     Expected = false,
-%%     Result = clientListener:is_restricted(lists,{seq,2}),
-%%     ?assertEqual(Expected,Result).
-
-%% function_is_restricted()->
-%%     %keep_Specs:add(lists,{seq,2}),
-%%     Expected = true,
-%%     Result = clientListener:is_restricted(lists,{seq,2}),
-%%     ?assertEqual(Expected,Result).
-
-%% runs_unrestricted_function()->
-%%     Expected = true,
-%%     Result = clientListener:is_restricted(lists,{seq,2}),
-%%     ?assertEqual(Expected,Result).
-%%      {"check execute list works ",fun check_execute_list/0},
-%%      {"check execute run works ",fun check_execute_run/0},
-%%      {"check execute info works  ",fun check_execute_info_works/0},
-%%      {"execute run fails when args are not proper",fun when_execute_run_should_crash/0},
- %%     {"check format with quoted variable",fun check_format_with_quoted_variables/0},
-%%{"check format with quoted string within variable ",fun to_erlang_string_2/0}
